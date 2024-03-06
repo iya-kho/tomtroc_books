14458074d01e4a3c02eb04051f328e06f5f98da4
Iya Shchur <iya.schur@gmail.com>
2024-03-06 10:27:42 +0200

    my profile front finished

--- views/templates/profile.php
+++ views/templates/profile.php
@@ -87,8 +87,9 @@
       <?php } ?>
       <!-- Show the user's books -->
       <?php if ($user->getBooksCount() > 0) { ?>
-      <div class="col col-lg-8">
-        <div class="d-lg-none row row-cols-1 row-cols-md-2 g-15">         
+      <div class="col <?= $profile==='private' ? 'col-lg-12' : 'col-lg-8' ?>">
+        <!-- Show the mobile version -->
+        <div class="d-lg-none row row-cols-1 g-15">         
           <?php foreach ($user->getBooks() as $book) : ?>
             <a href="index.php?action=book&id=<?= $book->getId() ?>">
               <div class="book-info col bg-white rounded p-50">
@@ -96,16 +97,32 @@
                   <img src="<?= $book->getImageUrl() ?>" alt="<?= $book->getTitle() ?>"
                     class="me-15"
                   >
-                  <div class="font-secondary fs-14">
-                    <span><?= $book->getTitle() ?></span>
-                    <span><?= $book->getAuthor() ?></span>
+                  <div class="font-secondary d-flex flex-column lh-1">
+                    <span class="fs-14"><?= $book->getTitle() ?></span>
+                    <span class="fs-14"><?= $book->getAuthor() ?></span>
+                    <?php if ($profile === 'private') { ?>
+                    <span class="availability fs-9 text-light py-5 px-10 mt-15 <?= $book->getAvailability() ? 'bg-success' : 'bg-danger' ?>">
+                      <?= $book->getAvailability() ? 'disponible' : 'non dispo.' ?>
+                    </span>
+                    <?php } ?>
                   </div>
                 </div>
                 <div class="font-secondary fs-14 fst-italic"><?= $book->getDescription(94) ?></div>
+                <?php if ($profile === 'private') { ?>
+                  <div class="d-flex mt-50">
+                    <a href="index.php?action=modifyBook&id=<?= $book->getId()?>" class="fs-16 text-decoration-underline me-40">
+                      Éditer
+                    </a>
+                    <a href="index.php?action=deleteBook&id=<?= $book->getId()?>" class="fs-16 text-decoration-underline delete-link">
+                      Supprimer
+                    </a>
+                  </div>
+                  <?php } ?>
               </div>
             </a>
           <?php endforeach; ?>
         </div>
+        <!-- Show the desktop version -->
         <div class="d-none d-lg-block rounded bg-white">
           <div class="row g-0 text-uppercase font-secondary fs-8 fw-semibold pt-30 pb-10 ps-65 pe-55 border-bottom border-dark">
             <div class="col">
@@ -116,30 +133,57 @@
             </div>
             <div class="col">
               auteur
-            </div>
+            </div> 
             <div class="col">
               description
             </div>
+            <?php if ($profile === 'private') { ?>
+            <div class="col">
+              disponibilité
+            </div>
+            <div class="col">
+              action
+            </div>
+            <?php } ?>
           </div>
           <?php foreach ($user->getBooks() as $index => $book) : ?>
-            <a href="index.php?action=book&id=<?= $book->getId() ?>" 
-              class="<?= $index % 2 === 0 ?
-            'row g-0 text-start align-items-center pt-25 pb-35 book-info ps-65 pe-55 font-secondary fs-12' :
-            'row g-0 text-start align-items-center pt-25 pb-35 book-info ps-65 pe-55 font-secondary fs-12 bg-info ' ?>
-            ">
-              <div class="col">
-                <img src="<?= $book->getImageUrl() ?>" alt="<?= $book->getTitle() ?>">
+            <?php if ($profile === 'public') { ?>
+            <a href="index.php?action=book&id=<?= $book->getId() ?>">
+            <?php } ?>
+              <div class="<?= $index % 2 === 0 ?
+              'row g-0 text-start align-items-center pt-25 pb-35 book-info ps-65 pe-55 font-secondary fs-12' :
+              'row g-0 text-start align-items-center pt-25 pb-35 book-info ps-65 pe-55 font-secondary fs-12 bg-info' ?>">
+                <div class="col">
+                  <img src="<?= $book->getImageUrl() ?>" alt="<?= $book->getTitle() ?>">
+                </div>
+                <div class="col">
+                  <?= $book->getTitle() ?>
+                </div>
+                <div class="col">
+                  <?= $book->getAuthor() ?>
+                </div>
+                <div class="col fst-italic">
+                  <?= $book->getDescription(82) ?>
+                </div>
+                <?php if ($profile === 'private') { ?>
+                <div class="col">
+                  <span class="availability fs-9 text-light py-5 px-10 <?= $book->getAvailability() ? 'bg-success' : 'bg-danger' ?>">
+                    <?= $book->getAvailability() ? 'disponible' : 'non dispo.' ?>
+                  </span>
+                </div>
+                <div class="col d-flex">
+                  <a href="index.php?action=modifyBook&id=<?= $book->getId()?>" class="fs-12 text-decoration-underline me-40">
+                    Éditer
+                  </a>
+                  <a href="index.php?action=deleteBook&id=<?= $book->getId()?>" class="fs-12 text-decoration-underline delete-link">
+                    Supprimer
+                  </a>
+                </div>
+                <?php } ?>
               </div>
-              <div class="col">
-                <?= $book->getTitle() ?>
-              </div>
-              <div class="col">
-                <?= $book->getAuthor() ?>
-              </div>
-              <div class="col fst-italic">
-                <?= $book->getDescription(82) ?>
-              </div>
+            <?php if ($profile === 'public') { ?>
             </a>
+            <?php } ?>
           <?php endforeach; ?>
         </div>
       </div>
